a190734195e24c55e19820bb5f91c3408385dde9
Camilo Tenjo <tenjoliscamilo@gmail.com>
2021-11-12 17:05:32 -0500

    Creacion de lista

---
+++ lib/Tiendas.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class Tiendas extends StatefulWidget{
+  @override
+  TiendasApp createState() => TiendasApp ();
+}
+class TiendasApp extends State<Tiendas>{
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSction=Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children:[
+          Expanded(child:
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom:8),
+                  child: Text(
+                    'Comidad Rapidas Crmen',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        )
+                  ),
+                ),
+                Text('Perros Calientes, Hamburguesas',
+                  style: TextStyle(
+                    color: Colors.red[500],
+                  ),
+
+                ),
+              ],
+          ),
+
+          ),
+          Container(
+            width: 80,
+              height: 80,
+            child: Image.asset('image/perro.png'),
+          ),
+          ElevatedButton(onPressed: (){}, child: Text('Entrar')),
+        ],
+      
+      ),
+    );
+    return Scaffold(
+    appBar: AppBar(
+    title: Text('Lista de Tiendas'),
+    ),
+      body: ListView(
+        children: [
+          titleSction,
+          titleSction
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:proyecto_1/Tiendas.dart';
 import 'buscar.dart';
 import 'package:flutter/src/widgets/icon.dart';
-//repo1
-void main(){
-    WidgetsFlutterBinding.ensureInitialized();
-    Firebase.initializeApp();
-    runApp(MyApp());
 
+//repo1
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Home())
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
   }
-class Home extends StatefulWidget{
+}
+
+class Home extends StatefulWidget {
   @override
   HomeStar createState() => HomeStar();
+}
 
-  }
-  @override
-  class HomeStar extends State<Home>{
+@override
+class HomeStar extends State<Home> {
   @override
   Widget build(BuildContext context) {
-  return MaterialApp(
-  title: 'Bienvenidos grupo 6',
-  home: Scaffold(
-  appBar: AppBar(
-
-  titleTextStyle: TextStyle(color: Colors.red.withOpacity(1.0)),
-  title: Text('Aplicacion'),
-
-  ),
-  body: SingleChildScrollView(
-  child: Column(
-  children: <Widget>[
-  Padding(
-  padding: EdgeInsets.only(left: 20,top: 20, right: 20, bottom: 0),
-  child: Center(
-  child:Container(
-  width: 200,
-  height: 200,
-  child: Image.asset('image/logo.png'),
-  ),
-  ),
-  ),
-  Padding(padding:EdgeInsets.only(left: 20,top: 40,right: 20,bottom: 0),
-  child:TextField(
-  decoration:InputDecoration(
-  labelText: 'Busqueda',
-  hintText: 'Palabra Clave'
-  ),
-  ),
-  ),
-  Padding(padding: EdgeInsets.only(left: 20, top: 20, right: 0,bottom: 0),
-  child: ElevatedButton(
-  onPressed:(){
-  print("presionando");
-  Navigator.push(context, MaterialPageRoute(builder: (_) => buscar()));
-  },
-  child:Text('Buscar'),
-  ),
-  ),
-  ],
-  ) ,
-  ),
-  ),
-  );
-  }
+    return MaterialApp(
+      title: 'Bienvenidos grupo 6',
+      home: Scaffold(
+        appBar: AppBar(
+          titleTextStyle: TextStyle(color: Colors.red.withOpacity(1.0)),
+          title: Text('Aplicacion'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding:
+                    EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 0),
+                child: Center(
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    child: Image.asset('image/logo.png'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.only(left: 20, top: 40, right: 20, bottom: 0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Busqueda', hintText: 'Palabra Clave'),
+                ),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.only(left: 20, top: 20, right: 0, bottom: 0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    print("presionando");
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => buscar()));
+                  },
+                  child: Text('Buscar'),
+                ),
+              ),
+              Padding(
+                padding:
+                EdgeInsets.only(left: 20, top: 20, right: 0, bottom: 0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    print("presionando");
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => Tiendas()));
+                  },
+                  child: Text('Lista de mercados'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
