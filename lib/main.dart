ba7a6c446a4c384428827e73dece14bd3d66738d
Camilo Tenjo <tenjoliscamilo@gmail.com>
2021-11-12 15:51:19 -0500

    r1

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'buscar.dart';
 import 'package:flutter/src/widgets/icon.dart';
+//repo1
 void main(){
     WidgetsFlutterBinding.ensureInitialized();
     Firebase.initializeApp();
